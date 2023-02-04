10f0d3f11ecee54da583284be253841fb33e80fd
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-02-04 22:13:00 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  nameId = nanoid();
+  numberId = nanoid();
+
+  handleNameChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  addContact = () => {
+    this.setState(
+      this.state.contacts.map(({ name, number }) => {
+        return `<li>${name}:${number}</li>`;
+      })
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.nameId}>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label htmlFor={this.numberId}>
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleNameChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <p>Contacts</p>
+        <ul></ul>
+      </div>
+    );
+  }
+}
